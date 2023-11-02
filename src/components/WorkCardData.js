beca8b48f63a0479d2afba583cf1cb65af8ffb34
3835swati <20BCS3835@cuchd.in>
2023-11-02 11:50:43 +0530

    changed

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -6,14 +6,16 @@ const ProjectCardData=[
         imgsrc: Furnisell ,
         title: "Ecommerce website Design",
         text: "FurniSell is a visually stunning e-commerce platform dedicated to showcasing and selling high-quality furniture. Our frontend design focuses on creating an engaging and intuitive shopping experience for users.",
-        view:"url.com"
+        view:"https://github.com/swati3835/Furniselll",
+        source:'https://github.com/swati3835/Furniselll'
     },
 
     {
         imgsrc: Amazone ,
         title: "Amazone Clone",
         text: "The Amazon Clone is a frontend web application that replicates the user interface and shopping experience of the popular e-commerce platform, Amazon. ",
-        view:"url.com"
+        view:"https://github.com/swati3835/Amzone",
+        source:'https://github.com/swati3835/Amzone'
     }
 ];
 
